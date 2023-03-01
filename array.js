311936c4544555b46e17181bd3cdf1d58fe98e9f
JulieSternfoss <u2440785@rts-365>
2023-03-01 10:25:42 +0100

    a

---
+++ array.js
@@ -0,0 +1,14 @@
+const number1 = ["1", "2", "3", "4", "6", "8",];
+const number2 = ["5", "6", "7", "8", "9", "10"];
+
+console.log(cars[0]);//is the first
+
+console.log(cars[1]);
+
+console.log(cars[2]);
+
+cars.push("tesla"); // "push" to add an element
+
+console.log(cars[3]);
+
+cars.pop(); //removes last element
