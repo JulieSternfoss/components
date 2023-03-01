468f83fc768580275b55e4a4828ec338b24c8893
JulieSternfoss <u2440785@rts-365>
2023-03-01 12:55:28 +0100

    a

--- array.js
+++ array.js
@@ -1,16 +1,8 @@
-var a; 
-console.log('hello world');
-
-//namber
-
-
 //arrays 
-let numbers = [ 1,2,3,4,6,8];
-
-const a1 = [1,2,3,4, 6,8];
+const a1 = [1,2,3,4,6,8];
 const a2 = [5,6,7,8,9,10];
 
-let r =[1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const r =[1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 console.log(r);
 
