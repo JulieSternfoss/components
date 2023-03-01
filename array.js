e7d98dff593af3c40a65621ba0b8519450c3aeee
JulieSternfoss <u2440785@rts-365>
2023-03-01 12:31:22 +0100

    a

--- array.js
+++ array.js
@@ -1,8 +1,16 @@
-//arrays 
-let numbers = [ 1,2,3,4, 6,8]
-//is thes okay?
-int a1[] = {1,2,3,4, 6,8};
-int a2[] = {5,6,7,8,9,10};
+var a; 
+console.log('hello world');
 
-int r[] ={1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+//namber
+
+
+//arrays 
+let numbers = [ 1,2,3,4,6,8];
+
+const a1 = [1,2,3,4, 6,8];
+const a2 = [5,6,7,8,9,10];
+
+let r =[1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+console.log(r);
 
