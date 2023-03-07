fa695e4b9e7945b1bb9fc875750ec1970f96b8c9
JulieSternfoss <u2440785@rts-365>
2023-03-07 10:28:39 +0100

    a

--- funks/funks.js
+++ funks/funks.js
@@ -1,6 +1,19 @@
-function skifterFarve(farve) {
+function skiftFarve(farve) {
 
-    document.querySelector(".skifterFarve").style.backgrundColor=farve;
+    document.querySelector(".skifterFarve").style.backgroundColor = farve;
     document.querySelector(".skifterFarve").style.color="white";
 }
-skifterFarve("green");
+skiftFarve("green");
+
+document.querySelector(".link1").addEventListener("click", function(){
+    skiftFarve("red");
+})
+document.querySelector(".link2").addEventListener("click", function(){
+    skiftFarve("blue");
+})
+document.querySelector(".link3").addEventListener("click", function(){
+    skiftFarve("pink");
+})
+document.querySelector(".link4").addEventListener("click", function(){
+    skiftFarve("purple");
+})
