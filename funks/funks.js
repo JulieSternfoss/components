d071f9a4f5021e3f9592bc0de9c0c38186d2fad1
JulieSternfoss <u2440785@rts-365>
2023-03-07 09:39:07 +0100

    a

---
+++ funks/funks.js
@@ -0,0 +1,6 @@
+function skifterFarve(farve) {
+
+    document.querySelector(".skifterFarve").style.backgrundColor=farve;
+    document.querySelector(".skifterFarve").style.color="white";
+}
+skifterFarve("green");
