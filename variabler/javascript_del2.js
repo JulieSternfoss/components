2e348decbf62336b91f6f88de70c016c04401c84
JulieSternfoss <u2440785@rts-365>
2023-03-09 11:32:34 +0100

    nyerettelser

---
+++ variabler/javascript_del2.js
@@ -0,0 +1,14 @@
+// var hej="Hej verden";
+
+// alert(hej);
+
+// var start="Hej "
+// var slut="verden"
+// var samlet="Hej varden"
+
+// document.write(samlet);
+
+var tallet1=4;
+var tallet2=6;
+var samlettallet=10;
+document.write(1+2);
