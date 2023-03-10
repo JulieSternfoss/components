b6ea38b9fce40019541bc7af164e915f4314cc39
JulieSternfoss <u2440785@rts-365>
2023-03-10 10:21:56 +0100

    gemt

--- variabler/javascript_del2.js
+++ variabler/javascript_del2.js
@@ -8,7 +8,64 @@
 
 // document.write(samlet);
 
-var tallet1=4;
-var tallet2=6;
-var samlettallet=10;
-document.write(1+2);
+// var tallet1=4;
+// var tallet2=6;
+// var samlettallet=10;
+// document.write(tallet1+tallet2);//kommentar: javascript ser at tallet1+tallet2 har en verdi på hhv 4 + 6 og vil derfor vise verdien 10
+
+// var tekst="dette er noget tekst"
+// var tal= 13456
+// var tilsammen="dette er noget tekst"+12356;
+
+// document.write(tekst+tal)
+
+
+//opgave 8 +9
+// var tal=prompt("skriv et tal")//prompt gir tekstfelt. write sættes til at være variablem og udskriver det som kommer ind i tekstfældet.
+// document.write(tal)
+// if(isNaN(false)){//isNaN tjekker om noget er et tal
+//     alert(tal+" er ikke et tal")
+// }
+// else{
+//     alert(tal +" er et tal- super!")
+// }
+
+//opgave 10
+// var tal=prompt("skriv et tal")
+// document.write(tal)
+// while (isNaN(tal)) {
+//     tal=prompt( tal + "dette er ikke et tal")
+//     alert("det er ikke et tal")
+// }
+
+//opgave 11
+// var tekst="hej"
+// var tal= 123
+// var bool=true
+// alert(typeof(bool));
+
+//opgave 12
+// var skrivnoget=prompt("skriv noget");
+// alert(typeof(skrivnoget));//den udskriver det hele som en "string"(som den jo også er);
+
+//opgave 13
+// var skrivnoget1=prompt("skriv noget");
+// var skrivnorget2=prompt("skriv noget andet her")
+// document.write(skrivnoget1+skrivnorget2);//variablerne skrivers efter hinanden uanset om det er tal eller tekst. 
+
+//opgave 14
+
+// var i=1
+// while(i < 5){
+//     document.write(i)
+// }
+
+//opgave 15
+var skrivnogether=prompt("skriv noget her")
+
+if(isNaN(true)){
+    alert(skrivnogether +" er ikke et tal")
+}
+else{
+ alert(skrivnogether +" er et tal- super!")
+};
