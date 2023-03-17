357b526df9175c9aeb9a94d4970f6b2014ca6900
JulieSternfoss <u2440785@rts-365>
2023-03-17 14:17:49 +0100

    a

---
+++ funks/zoo/script.js
@@ -0,0 +1,38 @@
+// var point = document.getElementById("point")
+// var pointAntal = 0;
+// point.innerText += pointAntal
+
+// document.getElementById("leaf").addEventListener("drop", dropHandler)
+
+// function test(){
+//     alert("FUCKFUCKFUCKFUCK")
+// }
+// function dragHandler (event) {
+//      event.target.style.left = event.x + "px"
+//       event.target.style.top = event.y + "px"
+//     }
+// function dropHandler (event){
+//     alert("FUCKFUCKFUCK")
+// }
+
+const drag =document.querySelector(".drop")
+
+const drop =document.querySelector(".drop")
+
+drag.addEventListener("dragend", dragHandler)
+
+drop.addEventListener("drop", dropHandler)
+drop.addEventListener("dragover", dragOverHandler)
+
+
+function dragHandler(event){
+    // alert("Dette er en alert")
+    event.target.style.left=event.x + "px"
+    event.target.style.top=event.y + "px"
+}
+function dropHandler(event){
+    alert("hurra!")
+}
+function dragOverHandler(event){
+    event.preventDefault()
+}
